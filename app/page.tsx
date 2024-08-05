c8e267667e90f57215a6fa167cbcc6b98b0190c2
Pradyuman Yadav <91771912+Champion00@users.noreply.github.com>
2024-08-05 07:37:34 +0000

    ok

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import HeroSection from "@/components/HeroSection";
+
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-black/[0.02]">
       <h1 className="text-2xl text-center">hello </h1>
+      <HeroSection />
     </main>
   );
 }
